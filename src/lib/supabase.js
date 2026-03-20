c4cd4468c46bf006239598c53050fe1942f5cef7
cwoo107 <woodscaleb@gmail.com>
2026-03-20 14:43:36 -0600

    autogenerate products from supabase

---
+++ src/lib/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+    'https://jiwpkocsfdkpxgsinnbr.supabase.co',
+    'sb_publishable_uKH8F4wpeVjfWzrGop1x5Q_6yNqJ99Y'
+);
